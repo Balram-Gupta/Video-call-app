fce02fa52a3c2d2406fd95c5fdb3672575c7c46a
Balram Gupta <balramgupta20101@gmail.com>
2026-05-18 23:38:39 +0530

    add backend link

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,10 +1,9 @@
-let IS_PROD = false;
+let IS_PROD = true;
+
 const localServer = `${window.location.protocol}//${window.location.hostname}:8000`;
 
-const server = IS_PROD ?
-    "https://video-call-app-2mgc.onrender.com" :
-
-    localServer
-
+const server = IS_PROD
+  ? "https://video-call-app-2mgc.onrender.com"
+  : localServer;
 
 export default server;
