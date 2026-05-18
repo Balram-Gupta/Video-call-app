96f4b37bbe74348e6f49ca630827f98209f9ca7e
Balram-Gupta <Balramgupta20101@gmail.com>
2026-05-18 23:12:43 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -2,7 +2,7 @@ let IS_PROD = false;
 const localServer = `${window.location.protocol}//${window.location.hostname}:8000`;
 
 const server = IS_PROD ?
-    "https://video-call-929x.onrender.com" :
+    "https://video-call-app-2mgc.onrender.com" :
 
     localServer
 
