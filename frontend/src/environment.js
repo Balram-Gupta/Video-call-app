33faeb5eebc4660aed31cdc81bece2f62e04af82
Balram Gupta <balramgupta20101@gmail.com>
2026-05-19 16:06:24 +0530

    environment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 const deployedServer = "https://video-call-app-2mgc.onrender.com";
 const localServer = "http://localhost:8000";
 
-const server = localServer || deployedServer;
+const server =   deployedServer || localServer;
 
 export const authApiUrl = `${server}/api/auth`;
 export default server;
